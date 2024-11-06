1a93559d88a405828907b842e5d114e211862b66
MATHEESHA <158041163+GPMATHEESHA@users.noreply.github.com>
2024-11-07 00:04:04 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Retirement',
+  meta: [{ name: 'description', content: 'Happy Retirement Life Sir!' }],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
